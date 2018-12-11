bb5ac8aa1569cdbab1ec700ddb717598758c45e7
nimeifu <gliu3637@bths.edu>
2018-12-11 10:11:25 -0500

    st

--- src/DeckClass.java
+++ src/DeckClass.java
@@ -1,8 +1,18 @@
 import java.util.ArrayList;
 
 public class DeckClass {
-    private ArrayList<Card>;
+    private ArrayList<Card> unDealt;
     private ArrayList<Card> Dealt;
 
-    public DeckClass()
+    public DeckClass(String[] ranks,String[] suits,int[] pointValues)
+    {
+
+      ArrayList<Object> deck=new ArrayList<Object>();
+      for(Object n:deck)
+      {
+          deck.add(ranks);
+          deck.add(suits);
+          deck.add(pointValues);
+      }
+    }
 }
